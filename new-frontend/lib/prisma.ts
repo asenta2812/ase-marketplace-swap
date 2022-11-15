595a18ec000641848ce5a227c8d3572960c72b46
Trong Nghia <52541823+asenta2812@users.noreply.github.com>
2022-11-16 02:12:40 +0700

    Update

--- new-frontend/lib/prisma.ts
+++ new-frontend/lib/prisma.ts
@@ -12,7 +12,9 @@ declare global {
 
 let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
+    prisma = new PrismaClient({
+        log: ['query', 'info', 'warn', 'error'],
+    });
 } else {
     if (!global.prisma) {
         global.prisma = new PrismaClient({
